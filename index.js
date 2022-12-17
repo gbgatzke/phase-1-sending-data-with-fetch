48754d3cc52af227544a7aa81a2197d8506da74b
Galen Beck Gatzke <gbgatzke@gmail.com>
2022-12-17 16:05:56 -0700

    commit tree 27fb3f169c89b61ea840143a09ebabd220711502

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your code here
+function submitData(name, email) {
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json",
+                    "Accept": "application/json"},
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+    })
+    .then(response => response.json())
+    .then(data => data.id.append());
+    
+}
